593c6a813de245c01f69fe80d7e7a137b4f80235
raphaeldanu <raphaeldanu@gmail.com>
2021-09-11 11:56:10 +0700

    Update Migrasi 1

--- database/migrations/2021_09_09_041547_create_student_reports_table.php
+++ database/migrations/2021_09_09_041547_create_student_reports_table.php
@@ -17,6 +17,28 @@ class CreateStudentReportsTable extends Migration
             $table->id();
             $table->foreignId('student_id');
             $table->integer('kelas');
+            $table->string('rombel', 1);
+            $table->integer('semester');
+            $table->string('tahun_pelajaran', 10);
+            $table->string('sikap_spiritual');
+            $table->text('deskripsi_sikap_spiritual');
+            $table->string('sikap_sosial');
+            $table->text('deskripsi_sikap_sosial');
+            $table->integer('n_agama');
+            $table->integer('n_ppkn');
+            $table->integer('n_bindo');
+            $table->integer('n_mat');
+            $table->integer('n_ipa');
+            $table->integer('n_ips');
+            $table->integer('n_bing');
+            $table->integer('n_seni');
+            $table->integer('n_penjas');
+            $table->integer('n_prakarya');
+            $table->integer('n_bjawa');
+            $table->integer('sakit');
+            $table->integer('izin');
+            $table->integer('tanpa_ket');
+            $table->text('catatan_wali_kelas');
             $table->timestamps();
         });
     }
