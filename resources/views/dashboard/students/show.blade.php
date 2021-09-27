76d1bce1732d89f65b93e207d197bb1f2544fd1f
raphaeldanu <raphaeldanu@gmail.com>
2021-09-27 09:21:41 +0700

    Update View

--- resources/views/dashboard/students/show.blade.php
+++ resources/views/dashboard/students/show.blade.php
@@ -4,6 +4,30 @@
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Siswa</h1>
   </div>
+  <div class="px-0 col-lg-8">
+    <div class="card border-0">
+      <div class="card-body px-0 pt-0">
+        <div class="row">
+          <div class="col-10">
+            <h4 class="card-title">{{ $student->nama }}</h4>
+          </div>
+          <div class="col-2">
+            <a href="/dashboard/students/" class="btn btn-danger">Kembali</a>
+          </div>
+        </div>
+        <p class="card-text">
+          <div class="row">
+            <div class="col-3">NISN</div>
+            <div class="col-9">: {{ $student->nisn }}</div>
+          </div>
+          <div class="row">
+            <div class="col-3">NIS</div>
+            <div class="col-9">: {{ $student->nis }}</div>
+          </div>
+        </p>
+      </div> 
+    </div>
+  </div>
   <div class="table-responsive col-lg-8">
     @if (session()->has('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
